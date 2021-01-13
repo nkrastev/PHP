9e14e90e58a992d864e727c6f6297325bc85c693
Nikolay Krastev <nkrastev@users.noreply.github.com>
2021-01-13 22:15:08 +0200

    Update index.php
    Query in the loop, clear the table before start

--- index.php
+++ index.php
@@ -1,42 +1,46 @@
 <?php
 require "lib/autoload.php";
 require "Config.php";
-use MailchimpAPI\Mailchimp;
 
+$mysqli = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE);
+mysqli_set_charset($mysqli, 'utf8mb4');
+$clearTheTable='TRUNCATE TABLE site_newsletters';
+$mysqli->query($clearTheTable);
+
+
+use MailchimpAPI\Mailchimp;
 $mailchimp = new Mailchimp($apiKey);
 
-$response = $mailchimp->campaigns()->get(["count" => 20, "sort_field"=>'create_time', 'sort_dir'=>'DESC']);
+$response = $mailchimp->campaigns()->get(["count" => 100, "sort_field"=>'create_time', 'sort_dir'=>'DESC']);
 $json = json_decode($response->getBody(), true);
 
 $camp = $response->deserialize()->campaigns;
 
-// foreach($camp as $items)
-// {
-//     if ($items->recipients->list_id==$englishList && $items->status=='sent')
-//     {
-//         echo $items->id." ".$items->status." ".$items->long_archive_url;
-//         echo "\n";
-//         echo $items->recipients->list_id;
-//         echo "\n";
-//         echo $items->settings->subject_line;
-
-//         echo "\n\n";
-//     }
-// }
-
-$mysqli = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE);
-mysqli_set_charset($mysqli, 'utf8mb4');
-//printf("Success... %s\n", mysqli_get_host_info($mysqli));
-
-$sql = "INSERT INTO site_newsletters (lang, subject, url) VALUES ('2', 'Test', 'tempUrl')";
-
-if ($mysqli->query($sql) === TRUE) 
+foreach($camp as $items)
+{
+    if ($items->status=='sent')
     {
-        echo "New record created successfully";
-    }
-    else
-    {
-        echo "Error: " . $sql . "<br>" . $mysqli->error;
-    }
+        //lang check depending on mailchimp list id
+        $langId=0;
+        if ($items->recipients->list_id==$englishList)
+        {
+            $langId=1;
+        } 
+        if ($items->recipients->list_id==$bulgarianList)
+        {
+            $langId=3;
+        }
+        if ($items->recipients->list_id==$danishList)
+        {
+            $langId=2;
+        }     
+        //prepare data
+        $dataSubject=$items->settings->subject_line;
+        $dataUrl=$items->long_archive_url;
 
-?>
+        //insert in DB
+
+        $sql = "INSERT INTO site_newsletters (lang, subject, url) VALUES ($langId, '$dataSubject', '$dataUrl')";
+        $mysqli->query($sql);
+    }
+}
