5ce393a6df1ed0ec88b297de39523a9aada0594c
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-26 16:50:51 +0200

    Hide category without deleting it
    Add code to function.php in website theme.

---
+++ Woocommerce/HideCategory.php
@@ -0,0 +1,22 @@
+<?php
+//hide category by name and by ID
+add_action( 'woocommerce_product_query', 'ts_custom_pre_get_posts_query' );
+function ts_custom_pre_get_posts_query( $q ) 
+{
+	$tax_query = (array) $q->get( 'tax_query' );
+	$tax_query[] = array(
+	'taxonomy' => 'product_cat',
+	'field' => 'slug',
+	'terms' =>array( 'Item1','Item2'), // Don't display products these categories
+	'operator' => 'NOT IN'
+	);
+	$q->set( 'tax_query', $tax_query );
+}
+
+add_filter( 'woocommerce_product_categories_widget_args', 'woo_product_cat_widget_args' );
+function woo_product_cat_widget_args( $cat_args ) {
+	$cat_args['exclude'] = array('000001'); //dont display the categories from these IDs, replace IDs
+	return $cat_args;
+}
+
+?>
