ed0c84c9d1359034f6491989e7bb96c11479c995
Nikolay Krastev <nkrastev@users.noreply.github.com>
2022-06-20 20:53:10 +0300

    Create DecryptData.php

---
+++ Cryptography/DecryptData.php
@@ -0,0 +1,18 @@
+function decryptData($dataField){
+	
+  $key = 'YourMegaGigaSecretKey';		
+	
+  $data = base64_decode($dataField);
+	$iv = substr($data, 0, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC));		
+	$decrypted_Data = rtrim(
+		mcrypt_decrypt(
+			MCRYPT_RIJNDAEL_128,
+			hash('sha256', $key, true),
+			substr($data, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC)),
+			MCRYPT_MODE_CBC,
+			$iv
+		),
+		"\0"
+	);
+	return $decrypted_Data; 
+}
